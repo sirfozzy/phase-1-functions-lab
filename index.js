4a63b42e83453d8b4376463d32ee6b4fd44d73a6
Jason Antman <antmanjt@gmail.com>
2022-03-25 10:55:20 -0400

    commit tree 46c7e82017b9688e2e81cff74c53aee6ae53d14b

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+const feet = 264;
+let beginning;
+let distance;
+let destination;
+let start;
+
+function distanceFromHqInBlocks (destination){
+    let start = 42;
+    if (destination > start)
+    return destination - start;
+
+    return start - destination;
+}
+
+function distanceFromHqInFeet(Blocks) {
+    let Feet = distanceFromHQInBlocks(Blocks)
+    return Feet * 264
+}
+
+function distanceTravelledInFeet (sLocation, eLocation) {
+    if (sLocation < eLocation)
+        return (eLocation - sLocation) * 264;
+    else
+        return (sLocation - eLocation) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+    if (destination - start <= 1)
+        return (start - destination +1)
+    else if (destination - start <= 8)
+        return 25
+    else
+        return ('cannot travel that far')
+}
